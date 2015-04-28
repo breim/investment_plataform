b340e5b291b439577ee3641595a155bf6562bd91
Henrique Breim <henrique@breim.com.br>
2015-04-28 17:34:58 -0300

    Add devise redirect after sign_in and permitted fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,35 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
+	helper_method :current_user
+
+
+	protected
+
+	# Configure permited fiels in rails
+	before_filter :configure_devise_params, if: :devise_controller?
+	def configure_devise_params
+		devise_parameter_sanitizer.for(:sign_up) do |u|
+			u.permit(:first_name,:last_name,:email,:password,:password_confirmation)
+		end
+
+		devise_parameter_sanitizer.for(:account_update) do |u|
+			u.permit(:first_name,:last_name,:email,:password)
+		end
+
+		devise_parameter_sanitizer.for(:sign_in) do |u|
+			u.permit(:email,:password,:password_confirmation)
+		end
+
+	end
+
+	# Redirect after sign_in user
+	def after_sign_in_path_for(resource)
+		case resource
+		when User
+			then 
+			projects_path 
+		else 
+			root_path
+	end
+
 end
